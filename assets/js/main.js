b3a4d6d66f52b8bcda145b06b11fb3ccb1ccd314
par0s <se.kidus.yoseph@gmail.com>
2021-02-22 12:22:03 +0300

    search video feature implemented on index html page

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+create('tour', {
+    name: "Fasilades",
+    location: "Gondar",
+    price: 480,
+    image: "/assets/images/gettyimages-138178737-2048x2048.jpg",
+    map: `<iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3893.608912203207!2d37.4674458148181!3d12.608006691095168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164327d5bd20f365%3A0xe39869932a279b76!2sFasil%20Ghebbi!5e0!3m2!1sen!2set!4v1613643085214!5m2!1sen!2set"
+    width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+    tabindex="0"></iframe>`,
+        
+    date: "2020-10-18"
+});
+create('tour', {
+    name: "Lalibela",
+    location: "Lalibela",
+    price: 280,
+    image: "/assets/images/gettyimages-111919734-2048x2048.jpg",
+    date: "2020-10-18"
+});
+create('tour', {
+    name: "Sof Umer Cave",
+    location: "Afar",
+    price: 550,
+    image: "/assets/images/gettyimages-182174818-2048x2048.jpg",
+    date: "2020-10-18"
+});
+create('tour', {
+    name: "Addis Ababa",
+    location: "Addis Ababa",
+    price: 350,
+    image: "/assets/images/gettyimages-697529054-612x612.jpg",
+    date: "2020-10-18"
+});
+create('tour', {
+    name: "Semen Mountains",
+    location: "Semen",
+    price: 520,
+    image: "/assets/images/gettyimages-905176238-2048x2048.jpg",
+    date: "2020-10-18"
+});
+create('tour', {
+            name: "Konso",
+            location: "Konso",
+            price: 600,
+});
